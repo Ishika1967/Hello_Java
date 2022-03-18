1c4e2a9a46ffa12a6f611b7772274143d34e1a8d
Ishika1967 <73094900+Ishika1967@users.noreply.github.com>
2022-03-18 15:46:38 +0530

    Created Hello.java

---
+++ Hello.java
@@ -0,0 +1,23 @@
+
+
+public class Main {
+    public static void printJava(){
+        System.out.println("HELLO JAVA");
+
+    }
+    public static void printNmae(String name){
+        System.out.println(name);
+    }
+
+
+    public static void main(String[] args) {
+	
+        
+       
+        System.out.println("hello JAVA");
+       printJava();
+       printJava();
+       printJava();
+       printNmae("ishika");
+        }
+    }
